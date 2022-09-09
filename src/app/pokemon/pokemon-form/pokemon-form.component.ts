98e66227d855552cbc0713cb55fcbf52127b4bbe
Thibault Berthelin <61558171+DevoWeb60@users.noreply.github.com>
2022-09-09 20:18:32 +0200

    Form

--- src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -38,7 +38,7 @@ export class PokemonFormComponent implements OnInit {
     isTypesValid(type: string): boolean {
         if (
             (this.pokemon.types.length == 1 && this.hasType(type)) ||
-            (this.pokemon.types.length >= 2 && !this.hasType(type))
+            (this.pokemon.types.length >= 3 && !this.hasType(type))
         ) {
             return false;
         }
